117767665f6305b03928ba40d2d6e1dc3a789831
Mochamadziad13 <ziadmochamad@gmail.com>
2023-03-10 11:02:53 +0700

    update

---
+++ P3/variable.php
@@ -0,0 +1,23 @@
+<?php
+
+$name = "Eko"
+$age = 30;
+
+echo "Name : "; 
+echo $name;
+echo "\n";
+
+echo "Age : ";
+echo $age;
+echo "\n";
+
+$contoh = "moch";
+$contoh = "ziad";
+
+echo "Contoh : ";
+echo $contoh;
+echo "n\";
+
+echo "moch : ";
+echo $moch;
+echo "n\";
